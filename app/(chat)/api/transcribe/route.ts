d55c79c1d3716713da4fd80507c13e79646c4a3f
inverted-capital <69952973+inverted-capital@users.noreply.github.com>
2024-11-28 01:37:17 +0000

    partial work on voice transcription

---
+++ app/(chat)/api/transcribe/route.ts
@@ -0,0 +1,32 @@
+import { assert } from 'node:console';
+import { auth } from '@/app/(auth)/auth';
+import OpenAI from 'openai';
+
+const apiKey = process.env.OPENAI_API_KEY;
+if (!apiKey) {
+  throw new Error('missing openai api key: OPENAI_API_KEY');
+}
+const ai = new OpenAI({ apiKey, maxRetries: 5 });
+
+export const maxDuration = 60;
+
+export async function POST(request: Request) {
+  const session = await auth();
+
+  if (!session || !session.user || !session.user.id) {
+    return new Response('Unauthorized', { status: 401 });
+  }
+
+  const formData = await request.formData();
+  const audio = formData.get('audio') as File;
+  assert(audio, 'audio is required');
+
+  const transcription = await ai.audio.transcriptions.create({
+    file: audio,
+    model: 'whisper-1',
+    prompt:
+      'Backchat, GPT4, GPT3, Dreamcatcher, CRM, HAL, Deno, Stucks, Redlid, Pijul',
+  });
+
+  return Response.json({ text: transcription.text }, { status: 200 });
+}
--- components/voice.tsx
+++ components/voice.tsx
@@ -1,8 +1,11 @@
+'use client';
+
 import 'regenerator-runtime/runtime';
+import { toast } from 'sonner';
 import { Button } from '@/components/ui/button';
 import { MicIcon } from 'lucide-react';
 import { cx } from 'class-variance-authority';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import SpeechRecognition, {
   useSpeechRecognition,
 } from 'react-speech-recognition';
@@ -14,8 +17,14 @@ interface VoiceProps {
 
 export function Voice({ input, onTranscription }: VoiceProps) {
   const [isRecording, setIsRecording] = useState(false);
-  const { transcript, resetTranscript } = useSpeechRecognition();
   const [initialInput, setInitialInput] = useState(input);
+  const { transcript, resetTranscript } = useSpeechRecognition();
+
+  const lastTranscriptTimeRef = useRef<Date | null>(null);
+
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const audioChunksRef = useRef<Blob[]>([]);
+
   useEffect(() => {
     if (isRecording) {
       setInitialInput(input);
@@ -24,6 +33,8 @@ export function Voice({ input, onTranscription }: VoiceProps) {
 
   useEffect(() => {
     if (isRecording) {
+      // Update the last transcript time whenever a new transcript arrives
+      lastTranscriptTimeRef.current = new Date();
       onTranscription(`${initialInput} ${transcript}`);
     }
   }, [transcript, isRecording, onTranscription, initialInput]);
@@ -32,10 +43,86 @@ export function Voice({ input, onTranscription }: VoiceProps) {
     if (isRecording) {
       resetTranscript();
       SpeechRecognition.startListening({ continuous: true });
+      startAudioRecording();
     } else {
       SpeechRecognition.stopListening();
+      stopAudioRecording();
     }
   }, [isRecording, resetTranscript]);
+
+  // Effect to monitor silence periods
+  // useEffect(() => {
+  //   let silenceInterval: NodeJS.Timeout;
+  //   if (isRecording) {
+  //     silenceInterval = setInterval(() => {
+  //       if (lastTranscriptTimeRef.current) {
+  //         const now = new Date();
+  //         const diff = now.getTime() - lastTranscriptTimeRef.current.getTime();
+  //         if (diff > 2000) {
+  //           // 2 seconds of silence detected
+  //           stopAudioRecording();
+  //           sendAudioToTranscribe();
+  //           // Reset for the next recording session
+  //           startAudioRecording();
+  //           lastTranscriptTimeRef.current = null;
+  //         }
+  //       }
+  //     }, 1000); // Check every second
+  //   }
+  //   return () => clearInterval(silenceInterval);
+  // }, [isRecording]);
+
+  const startAudioRecording = async () => {
+    if (navigator.mediaDevices?.getUserMedia) {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+      });
+      mediaRecorderRef.current = new MediaRecorder(stream);
+      mediaRecorderRef.current.start();
+
+      mediaRecorderRef.current.ondataavailable = (e) => {
+        audioChunksRef.current.push(e.data);
+      };
+    } else {
+      console.error('getUserMedia is not supported on your browser!');
+    }
+  };
+
+  const stopAudioRecording = () => {
+    if (mediaRecorderRef.current) {
+      mediaRecorderRef.current.stop();
+      mediaRecorderRef.current.stream.getTracks().forEach((track) => {
+        track.stop();
+      });
+    }
+  };
+
+  const sendAudioToTranscribe = async () => {
+    const audioBlob = new Blob(audioChunksRef.current, { type: 'audio/webm' });
+    audioChunksRef.current = []; // Reset for next recording
+
+    // Prepare form data for API request
+    const formData = new FormData();
+    formData.append('audio', audioBlob, 'audio.webm');
+
+    const transcribe = fetch('/api/transcribe', {
+      method: 'POST',
+      body: formData,
+    });
+    toast.promise(transcribe, {
+      loading: 'Transcribing...',
+      success: async (response) => {
+        const data = await response.json();
+        if (data.text) {
+          // Replace the current transcription with the high-quality transcription
+          onTranscription(`${initialInput} ${data.text}`);
+        }
+
+        return 'Transcribed!';
+      },
+      error: 'Failed to transcribe.',
+    });
+  };
 
   return (
     <Button
